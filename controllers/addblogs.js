2523718e55d6a01d2ae28c34d6dc21178dbb76cb
Naveen Sharma <ns222280@gmail.com>
2024-05-22 11:26:38 +0530

    getBlogs api is ready

--- controllers/addblogs.js
+++ controllers/addblogs.js
@@ -1,78 +1,101 @@
 const cloudnary = require("cloudinary").v2;
+const { Op } = require("sequelize");
 const blogSchema = require("../models/blogs");
 
 function isFileTypeSupported(type, supportedTypes) {
-    return supportedTypes.includes(type);
-  }
-
-  async function uploadFileToCloudinary(file, folder, quality) {
-    const options = { folder };
-  
-    if (quality) {
-      options.quality = quality;
-    }
-  
-    options.resource_type = "auto";
-    return await cloudnary.uploader.upload(file.tempFilePath, options);
-  }
-
-
-exports.uploadBlogs = async (req, res)=>{
-    try{
-        const blogImg = req.files.imageFile;
-        console.log(blogImg)
-
-        const {title, description, content, category } = req.body;
-        console.log({
-            title,
-            description,
-            content,
-            category
-        });
-        //validation
-
-        const supportedTypes = ["jpg", "jpeg", "png"];
-        const fileType = blogImg.name.split(".")[1].toLowerCase();
-    
-        if (!isFileTypeSupported(fileType, supportedTypes)) {
-          return res.status(400).json({
-            success: false,
-            message: "file format not supported",
-          });
-        }
-
-        const respone = await uploadFileToCloudinary(blogImg, "naveenCode");
-        
-        await blogSchema.create({
-            blogImage:respone.secure_url,
-            title,
-            description,
-            content,
-            category
-        })
-
-        return res.status(200).json({
-            success: true,
-            message: "Blog Created Successfully",
-          });
-
-    }catch(err){
-        console.log(err.message)
-        return res.status(500).json({
-          success: false,
-          message: "Internal server error",
-        });
-    }
+  return supportedTypes.includes(type);
 }
 
-exports.getBlogs = async (req, res)=>{
-  try{
-    
-  }catch(err){
-    console.log(err.message)
-          return res.status(500).json({
-          success: false,
-          message: "Internal server error",
-        });
+async function uploadFileToCloudinary(file, folder, quality) {
+  const options = { folder };
+
+  if (quality) {
+    options.quality = quality;
   }
+
+  options.resource_type = "auto";
+  return await cloudnary.uploader.upload(file.tempFilePath, options);
 }
+
+exports.uploadBlogs = async (req, res) => {
+  try {
+    const blogImg = req.files.imageFile;
+    console.log(blogImg);
+
+    const { title, description, content, category } = req.body;
+    console.log({
+      title,
+      description,
+      content,
+      category,
+    });
+    //validation
+
+    const supportedTypes = ["jpg", "jpeg", "png"];
+    const fileType = blogImg.name.split(".")[1].toLowerCase();
+
+    if (!isFileTypeSupported(fileType, supportedTypes)) {
+      return res.status(400).json({
+        success: false,
+        message: "file format not supported",
+      });
+    }
+
+    const respone = await uploadFileToCloudinary(blogImg, "naveenCode");
+
+    await blogSchema.create({
+      blogImage: respone.secure_url,
+      title,
+      description,
+      content,
+      category,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Blog Created Successfully",
+    });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+exports.getBlogs = async (req, res) => {
+  try {
+    const { query } = req.params;
+
+    if (query !== "all") {
+      const result = await blogSchema.findAll({
+        where: {
+          category: {
+            [Op.like]: `%${query}%`,
+          },
+        },
+      });
+      return res.status(200).json({
+        success: true,
+        result,
+        message: "data fetched successfully",
+      });
+    } else {
+
+      const result = await blogSchema.findAll();
+      return res.status(200).json({
+        success: true,
+        result,
+        message: "data fetched successfully",
+      });
+    }
+
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
--- routers/routes.js
+++ routers/routes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router =  express.Router();
 const {sendOtp, login, signup} = require("../controllers/auth");
-const {uploadBlogs} = require("../controllers/addblogs");
+const {uploadBlogs, getBlogs} = require("../controllers/addblogs");
 
 // router.get("/sendmail", sendmailyup)
 router.post("/sendotp", sendOtp);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/addBlog", uploadBlogs)
+router.get("/getBlogs/query=:query", getBlogs)
 
 
 module.exports = router
